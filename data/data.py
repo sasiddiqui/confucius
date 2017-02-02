7be5b23950367ef4caea277641557ed65ba2853f
porkyfly <tylerli800@gmail.com>
2017-02-02 16:33:37 -0500

    blahblah

--- data/data.py
+++
@@ -1,9 +0,0 @@
-# process data
-
-import json
-from pprint import pprint
-
-with open("scifi.json") as scifi:
-    items = json.loads(scifi)
-    scifi.close()
-
---
+++ data/getNLCdata.py
@@ -0,0 +1,22 @@
+#Tyler
+
+# process data
+import json
+from pprint import pprint
+
+writeFile = open('NLCtrain', 'w')
+
+#list of json files to format, with the filename = NLC classname
+list_of_files = ['python.json', 'scifi.json']
+
+for f in list_of_files:
+    with open(f) as file:
+        json_data = file.read()
+        items = json.loads(json_data)
+
+        #trim the .json off
+        trimmed = f.split('.')[0]
+
+        for item in items:
+            print(item['title'] + ',' + trimmed, file=writeFile)
+
