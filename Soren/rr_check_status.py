c25717e67987257ee32a860cfb02d9063aa4ddef
Soren Goyal <sorengoyal@gmail.com>
2017-02-07 20:03:37 -0500

    Wrote the scripts for RR

--- Soren/rr_check_status.py
+++ Soren/rr_check_status.py
@@ -8,23 +8,43 @@ Created on Tue Feb  7 16:07:57 2017
 @author: soren
 """
 #%%
+import sys
+import time
 import json
 from watson_developer_cloud import RetrieveAndRankV1
-credentials_string = '''{
-  "url": "https://gateway.watsonplatform.net/retrieve-and-rank/api",
-  "password": "ANlCCDTtOHr4",
-  "username": "55729e6b-2d18-46f8-b6a0-3e87cdd80797"
-}'''
-credentials = json.loads(credentials_string)
+with open('rr-config.json') as rr_config:
+    config = json.load(rr_config)
 retrieve_and_rank = RetrieveAndRankV1(
-    username = credentials['username'],
-    password= credentials['password'])
+    username = config['credentials']['username'],
+    password= config['credentials']['password'])
 #%%
-
-solr_clusters = retrieve_and_rank.list_solr_clusters()
-print(json.dumps(solr_clusters, indent=2))
-solr_cluster_id = 'sc00f69555_6065_4fac_81d9_6d73bec5d006'
-status = retrieve_and_rank.get_solr_cluster_status(solr_cluster_id=solr_cluster_id)
+cluster_name = config['solr_cluster_name']
+solr_clusters_list = retrieve_and_rank.list_solr_clusters()
+print(json.dumps(solr_clusters_list, indent=2))
+exists = False
+solr_cluster = {}
+for item in solr_clusters_list['clusters']:
+    if(item['cluster_name'] == cluster_name):
+        exists = True
+        solr_cluster = item
+        break
+if(exists):
+    print(json.dumps(solr_cluster, indent=2))
+    print('Cluster exists')
+    solr_cluster_id = solr_cluster['solr_cluster_id']
+else:
+    print('Cluster does not exist')
+    sys.exit()
 #%%
 configs = retrieve_and_rank.list_configs(solr_cluster_id=solr_cluster_id)
 print(json.dumps(configs, indent=2))
+#%%
+collections = retrieve_and_rank.list_collections(solr_cluster_id=solr_cluster_id)
+print(json.dumps(collections, indent=2))
+
+#%%
+pysolr_client = retrieve_and_rank.get_pysolr_client(solr_cluster_id, 'AI')
+results = pysolr_client.search("j.app.mech. 3,1947, a269.")
+print('{0} documents found'.format(len(results.docs)))
+for item in results.docs:
+    print(item['id'])
