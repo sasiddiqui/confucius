fd2c94282ec7564c34c1b2917d65f5e7489ef9ad
wizhang <william1994z@yahoo.com>
2017-02-13 23:13:45 -0500

    Test Changes

--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -16,3 +16,17 @@ class StandAloneTests(TestCase):
         mock_open.return_value.read.return_value = \
             "george|bosco\n"
         self.assertTrue(auth.login('george', 'bosco'))
+
+
+    @patch('__builtin__.open')
+    def test_login_bad_creds(self, mock_open):
+        """Test the login function when bad creds are passed."""
+        mock_open.return_value.read.return_value = \
+            "george|bosco"
+        self.assertFalse(auth.login('george', 'marbleRye'))
+
+    @patch('__builtin__.open')
+    def test_login_error(self, mock_open):
+        """Test the login function when an error happens."""
+        mock_open.side_effect = IOError()
+        self.assertFalse(auth.login('george', 'bosco'))
