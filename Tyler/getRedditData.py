e4a7391a39e52b8528520ab4f507a5ea7c64dea6
porkyfly <tylerli800@gmail.com>
2017-02-28 18:35:30 -0500

    Merge branch 'master' of https://github.com/sasiddiqui/confucius
    # Conflicts:
    #	main/demo.py

--- Tyler/getRedditData.py
+++ Tyler/getRedditData.py
@@ -10,51 +10,69 @@ limit = 10  #how many questions
 category = 'science' #DONT FORGET TO CHANGE THIS
 retrieveFile = 'redditRetrieve' + subreddit + '.json'
 NLCfile = 'redditNLC.csv'
+RankerFile = 'redditRanker' + subreddit + '.csv'
+commentThresh = 5
 
 
 #PRAW credentials
 reddit = praw.Reddit(client_id='XarwttTqbYFghg',
                      client_secret='OzsZALZ9430Avf_jV83uq3Mxn10',
                      user_agent='my user agent')
-titles = []
 nlcData = []
+rankerData = []
 retrieveData = {}
 retrieveData['documents'] = []
 
-idcount = 0
 for submission in reddit.subreddit(subreddit).top(limit=limit):
 
     title = submission.title
-    titles.append(title)
 
+    #get all the top level comments in that submission
     comments = []
     for comment in submission.comments:
         if isinstance(comment, MoreComments):
             continue
         comments.append(comment)
 
+    #sort the comments by upvotes
     comments.sort(key=lambda comment: comment.score, reverse=True)
 
-    if len(comments):
-        ans = comments[0].body
+    #store the top n comments
+    topScore = 0
+    topComments = []  # each comment should be comment, score, id
+    if len(comments) > commentThresh:
+        for k in range(commentThresh):
+            topComments.append([comments[k].body, comments[k].score, comments[k]])
+
+    #comments[2] = answerID
+    #comments[1] = answerScore
 
     #store data to write to NLC
-    if len(title) + len(ans) <= 1024:
+    if len(title) + len(category) <= 1024:
         nlcData.append(title + ' , ' + category)
 
-    #store data to write to the retriever
-    retrieveData['documents'].append({'id': idcount,'body': {'question': submission.title,'answer': ans}})
+    answerSequence = '' #answerid, answerscore
+    for commentEntry in topComments:
+        # store data to write to the retriever
+        retrieveData['documents'].append({'id': str(commentEntry[2]),'body': {'question': title,'answer': commentEntry[0]}})
 
-    #increment id
-    idcount += 1
+        # store data to write to the ranker
+        answerSequence = answerSequence + ',' + str(commentEntry[2]) + ',' + str(commentEntry[1])
 
-#write to NLCData
+    rankerData.append(title + answerSequence)
+
+#write to NLCDataFile
 f= open(NLCfile, 'a')
 for line in nlcData:
     print(line.encode('utf8'), file=f)
 f.close()
 
-#write to retrieveData
+#write to retrieveDataFile
 retriever = open(retrieveFile, 'w+')
 json.dump(retrieveData, retriever, indent=4)
 
+#write to rankerDataFile
+f2 = open(RankerFile, 'w+')
+for line in rankerData:
+    print(line.encode('utf8'), file=f2)
+f2.close()
