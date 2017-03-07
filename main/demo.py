66d75061114f032e17cb9d306e31dd21516655b6
porkyfly <tylerli800@gmail.com>
2017-03-06 20:09:29 -0500

    working demo 3 with reddit data, but retrieve only.

--- main/demo.py
+++ main/demo.py
@@ -17,14 +17,15 @@ def classify(id, question):
 def retrieveRank(question, topic):
     with open('rr-config.json') as credentials_file:
         credentials = json.load(credentials_file)
+
     retrieve_and_rank = RetrieveAndRankV1(
         username=credentials['credentials']['username'],
         password=credentials['credentials']['password'])
-    # Replace with your own solr_cluster_id
-    solr_clusters = retrieve_and_rank.list_solr_clusters()
-    solr_cluster_id = solr_clusters['clusters'][0]['solr_cluster_id']
 
-    pysolr_client = retrieve_and_rank.get_pysolr_client(solr_cluster_id, topic.upper())
+    solr_clusters = retrieve_and_rank.list_solr_clusters()
+    solr_cluster_id = solr_clusters['clusters'][1]['solr_cluster_id']
+
+    pysolr_client = retrieve_and_rank.get_pysolr_client(solr_cluster_id, topic)
     # Can also refer to config by name
 
     # Example search
@@ -35,32 +36,32 @@ def retrieveRank(question, topic):
     return results.docs[0]['body'][0]
     #return results.docs[0]['body'][0].split(',')[0].split(':')[1]
 
-def get_topic(question):
-    topic = classify('f5b432x172-nlc-3555', question)
-    return topic
-
-def get_answer(question):
-    topic = classify('f5b432x172-nlc-3555', question)
-    answer = retrieveRank(question, topic).replace("\\n", "").replace("u'","").replace("answer':", "")
-    return answer
+# def get_topic(question, id):
+#     topic = classify(id, question)
+#     return topic
+#
+# def get_answer(question, id):
+#     topic = classify(id, question)
+#     answer = retrieveRank(question, topic).replace("\\n", "").replace("u'","").replace("answer':", "")
+#     return answer
 
 if __name__ == '__main__':
     #get the question
     question = raw_input('ask your question: ')
 
     #classify the question
-    topic = classify('f5b432x172-nlc-3555', question)
-
+    topic = classify('90e7acx197-nlc-170', question)
     print 'the topic is {0}'.format(topic)
 
     answer = retrieveRank(question, topic).replace("\\n", "").replace("u'","").replace("answer':", "")
-
     print answer
+    print('')
 
     response = raw_input('is this what you were looking for?')
     if response == 'no':
         newTopic = raw_input('please enter the new topic')
         answer = retrieveRank(question, newTopic.replace("\\n", "").replace("u'", "").replace("answer':", ""))
+        print answer
 
-    print answer
+
 
